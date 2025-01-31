2b5aea4045d5e2eb682007328daf4ec250474024
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-01-31 12:41:57 +0500

    Add lucide-react and few changes in config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      exclude: ['lucide-react']
+    }
   }
 })
